5f95eeec337efa351a79c5a926d07974538682a3
J.U.!.C.£ <joaovitor20071220@gmail.com>
2025-03-27 10:44:50 -0300

    Pronto2

---
+++ tipo/Program.cs
@@ -0,0 +1,40 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        //tipos numericos
+        //numero do tipo inteiro
+        int numeroInt = 104;
+
+
+        //numeros decimais
+        //float - decimais com uma menor precisão
+        float numeroFloat = (float)13.333;
+
+
+        //double - decimais com precisão media 
+        double numeroDouble = (double)10.333;
+        double numeroDouble2 = 101;
+
+
+        
+        //decimal - decimais com precisão
+        decimal numeroDecimal = (decimal)1000.1231231231231312312;
+
+        //tipos textuais 
+
+        //caractere - valotes com no max. 1 posição
+        //aspas('') simples
+        char caractere = 'F';
+        char caractere2 = (char)9;
+        char caractere3 = '?';
+
+        char sexo = 'F';
+        char sexo2 = 'M';
+
+        //string
+        //aspas("") duplas
+        string texto = "FAZ O L";
+        string texto2 = "A";
+    }
+}
