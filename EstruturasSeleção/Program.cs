2b5ee28e9fee9534105708e0c55b7b0d6b0e6206
J.U.!.C.£ <joaovitor20071220@gmail.com>
2025-04-02 09:36:59 -0300

    estrutura de seleção

---
+++ EstruturasSeleção/Program.cs
@@ -0,0 +1,30 @@
+﻿class Program
+{
+
+    public static void Main()
+    {
+        
+        Console.WriteLine("Qual é sua idade");
+        int idade = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Comando 1");
+
+
+        //if realiza operações que retorna verdadeiro ou falso
+        //true - verdadeiro 
+        //falso - falso
+
+        if (idade >= 18 )
+        {
+            Console.WriteLine("É maior de idade/Verdadeiro");
+        }
+        else 
+        { 
+            Console.WriteLine("É menor de idade/Falso");
+        }
+
+
+
+       
+    }
+}
